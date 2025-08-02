346ef4c0b84a767faa69563d9caab0e5d5569f8d
n.ryu <ababc100564@gmail.com>
2025-08-02 19:40:55 +0900

    feat: gameState layer done

--- packages/core/src/gameState.test.ts
+++ packages/core/src/gameState.test.ts
@@ -1,12 +1,35 @@
-import * as calculator from "./calculator";
-import { type GameState, moveRunners, rollDice } from "./gameState";
+import {
+	betGame,
+	betRound,
+	type GameState,
+	isOnEffectTile,
+	moveRunners,
+	resolveDiceRollReward,
+	resolveEffectTileReward,
+	resolveGameBetReward,
+	resolvePartnershipReward,
+	resolveRoundBetReward,
+	rollDice,
+	setEffectTile,
+	setPartnership,
+} from "./gameState";
 
 const gameState: GameState<"red" | "blue", "red" | "green"> = {
+	initialTrackLength: 0,
+	gameBetRewards: {
+		first: [],
+		last: [],
+	},
+	gameBetPenalty: {
+		first: -1,
+		last: -1,
+	},
 	tracks: [],
 	dices: {
 		unrolled: [],
 		rolled: [],
 	},
+	rollTokens: [],
 	gameBetting: {
 		first: [],
 		last: [],
@@ -23,9 +46,10 @@ const player: (typeof gameState)["players"][number] = {
 	money: 0,
 	gameBetting: [],
 	roundBetting: [],
+	hasEffectTile: true,
 	isActive: false,
 	isRoundLead: false,
-	rollCount: 0,
+	rollTokens: [],
 };
 
 it("moveRunners", () => {
@@ -49,78 +73,426 @@ it("rollDice", () => {
 	jest.spyOn(Math, "random").mockReturnValueOnce(1 / 3 + 0.0001);
 
 	expect(
-		rollDice("player-0", {
-			...gameState,
-			players: [{ ...player }],
-			dices: {
-				unrolled: [
-					{
-						color: "red",
-						options: [
-							{ runnerColor: "red", value: 1 },
-							{ runnerColor: "red", value: 2 },
-							{ runnerColor: "red", value: 3 },
-						],
-					},
-					{
-						color: "green",
-						options: [
-							{ runnerColor: "blue", value: 1 },
-							{ runnerColor: "blue", value: 2 },
-							{ runnerColor: "blue", value: 3 },
-						],
-					},
-				],
-				rolled: [],
+		rollDice(
+			{
+				...gameState,
+				players: [{ ...player }],
+				rollTokens: [1, 2, 3],
+				dices: {
+					unrolled: [
+						{
+							color: "red",
+							options: [
+								{ runnerColor: "red", value: 1 },
+								{ runnerColor: "red", value: 2 },
+								{ runnerColor: "red", value: 3 },
+							],
+						},
+						{
+							color: "green",
+							options: [
+								{ runnerColor: "blue", value: 1 },
+								{ runnerColor: "blue", value: 2 },
+								{ runnerColor: "blue", value: 3 },
+							],
+						},
+					],
+					rolled: [],
+				},
 			},
-		}),
+			"player-0",
+		),
 	).toEqual({
-		gameState: {
-			...gameState,
-			players: [{ ...player, rollCount: 1 }],
-			dices: {
-				unrolled: [
-					{
-						color: "red",
-						options: [
-							{ runnerColor: "red", value: 1 },
-							{ runnerColor: "red", value: 2 },
-							{ runnerColor: "red", value: 3 },
-						],
-					},
-				],
-				rolled: [
-					{
-						color: "green",
-						prevResult: { runnerColor: "blue", value: 2 },
-					},
-				],
-			},
-		},
-		diff: {
-			dice: { color: "green", prevResult: { runnerColor: "blue", value: 2 } },
-			player: { id: "player-0", rollCount: 1 },
+		...gameState,
+		players: [{ ...player, rollTokens: [3] }],
+		rollTokens: [1, 2],
+		dices: {
+			unrolled: [
+				{
+					color: "red",
+					options: [
+						{ runnerColor: "red", value: 1 },
+						{ runnerColor: "red", value: 2 },
+						{ runnerColor: "red", value: 3 },
+					],
+				},
+			],
+			rolled: [
+				{
+					color: "green",
+					prevResult: { runnerColor: "blue", value: 2 },
+				},
+			],
 		},
 	});
 });
 
-it("resolveDiceRollReward", () => {});
+it.todo("rollDice throws an error if no such player exists");
+it.todo("rollDice throws an error if no unrolled dice remains");
+it.todo("rollDice throws an error if no rollToken remains");
 
-it("setEffectTile", () => {});
-it("resolveEffectTileReward", () => {});
-it("isOnEffectTile", () => {});
+it("resolveDiceRollReward", () => {
+	expect(
+		resolveDiceRollReward({
+			...gameState,
+			players: [
+				{ ...player, id: "player-0", money: 1, rollTokens: [1, 2] },
+				{ ...player, id: "player-1", money: 10, rollTokens: [3] },
+			],
+		}),
+	).toEqual({
+		...gameState,
+		players: [
+			{ ...player, id: "player-0", money: 4, rollTokens: [] },
+			{ ...player, id: "player-1", money: 13, rollTokens: [] },
+		],
+	});
+});
 
-it("betRound", () => {});
-it("resolveRoundBetReward", () => {});
+it("setEffectTile", () => {
+	expect(
+		setEffectTile(
+			{
+				...gameState,
+				tracks: [{ runners: ["red"] }, { runners: ["blue"] }, { runners: [] }],
+				players: [{ ...player, hasEffectTile: true }],
+			},
+			"player-0",
+			2,
+			-1,
+		),
+	).toEqual({
+		...gameState,
+		tracks: [
+			{ runners: ["red"] },
+			{ runners: ["blue"] },
+			{ runners: [], effectTile: { owner: "player-0", type: -1 } },
+		],
+		players: [{ ...player, hasEffectTile: false }],
+	});
+});
 
-it("betGame", () => {});
-it("resolveRoundBetReward", () => {});
+it("setEffectTile moves existing effectTile if it is already set", () => {
+	expect(
+		setEffectTile(
+			{
+				...gameState,
+				tracks: [
+					{ runners: ["red"] },
+					{ runners: [], effectTile: { owner: "player-0", type: 1 } },
+					{ runners: [] },
+				],
+				players: [{ ...player, hasEffectTile: false }],
+			},
+			"player-0",
+			2,
+			-1,
+		),
+	).toEqual({
+		...gameState,
+		tracks: [
+			{ runners: ["red"] },
+			{ runners: [] },
+			{ runners: [], effectTile: { owner: "player-0", type: -1 } },
+		],
+		players: [{ ...player, hasEffectTile: false }],
+	});
+});
 
-it("setPartnership", () => {});
-it("resolvePartnershipReward", () => {});
+it.todo("setEffectTile throws an error if no such player exists");
+it.todo(
+	"setEffectTile throws an error if no such track with given index exists",
+);
+it.todo("setEffectTile throws an error if target track has runners on it");
+it.todo(
+	"setEffectTile throws an error if target track has adjacent track with effect tile",
+);
 
-it("isRoundEnded", () => {});
+it("isOnEffectTile returns index of track having runner on the effect tile", () => {
+	expect(
+		isOnEffectTile({
+			...gameState,
+			tracks: [
+				{ runners: ["red"] },
+				{ runners: ["blue"], effectTile: { owner: "player-0", type: 1 } },
+				{ runners: [] },
+			],
+		}),
+	).toEqual({ value: true, index: 1 });
+});
 
-it("isGameEnded", () => {});
+it("isOnEffectTile returns value with false when no track has runner on the effect tile", () => {
+	expect(
+		isOnEffectTile({
+			...gameState,
+			tracks: [
+				{ runners: ["red"] },
+				{ runners: [], effectTile: { owner: "player-0", type: 1 } },
+				{ runners: [] },
+			],
+		}),
+	).toEqual({ value: false, index: undefined });
+});
 
-it("endGame", () => {});
+it("resolveEffectTileReward", () => {
+	expect(
+		resolveEffectTileReward({
+			...gameState,
+			tracks: [
+				{ runners: ["red"] },
+				{ runners: ["blue"], effectTile: { owner: "player-0", type: 1 } },
+				{ runners: [] },
+			],
+			players: [{ ...player, id: "player-0", money: 1 }],
+		}),
+	).toEqual({
+		...gameState,
+		tracks: [
+			{ runners: ["red"] },
+			{ runners: ["blue"], effectTile: { owner: "player-0", type: 1 } },
+			{ runners: [] },
+		],
+		players: [{ ...player, id: "player-0", money: 2 }],
+	});
+});
+
+it.todo(
+	"resolveEffectTileReward throws an error if no track has runner on the effect ",
+);
+
+it("betRound", () => {
+	expect(
+		betRound(
+			{
+				...gameState,
+				players: [{ ...player, id: "player-0", roundBetting: [] }],
+				roundBetting: {
+					red: [
+						{ color: "red", value: [2, 1, -1] },
+						{ color: "red", value: [3, 1, -1] },
+					],
+					blue: [{ color: "blue", value: [2, 1, -1] }],
+				},
+			},
+			"player-0",
+			"red",
+		),
+	).toEqual({
+		...gameState,
+		players: [
+			{
+				...player,
+				id: "player-0",
+				roundBetting: [{ color: "red", value: [3, 1, -1] }],
+			},
+		],
+		roundBetting: {
+			red: [{ color: "red", value: [2, 1, -1] }],
+			blue: [{ color: "blue", value: [2, 1, -1] }],
+		},
+	});
+});
+it.todo("betRound throws error if no betting card remains for desired color");
+
+it("resolveRoundBetReward", () => {
+	expect(
+		resolveRoundBetReward({
+			...gameState,
+			roundBetting: {
+				red: [],
+				blue: [],
+			},
+			tracks: [{ runners: [] }, { runners: ["blue", "red"] }, { runners: [] }],
+			players: [
+				{
+					...player,
+					id: "player-0",
+					money: 10,
+					roundBetting: [
+						{ color: "red", value: [3, -1] },
+						{ color: "red", value: [1, -1] },
+						{ color: "blue", value: [3, 1, -1] },
+					],
+				},
+				{
+					...player,
+					id: "player-1",
+					money: 10,
+					roundBetting: [{ color: "blue", value: [-1] }],
+				},
+			],
+		}),
+	).toEqual({
+		...gameState,
+		roundBetting: {
+			red: [
+				{ color: "red", value: [1, -1] },
+				{ color: "red", value: [3, -1] },
+			],
+			blue: [
+				{ color: "blue", value: [-1] },
+				{ color: "blue", value: [3, 1, -1] },
+			],
+		},
+		tracks: [{ runners: [] }, { runners: ["blue", "red"] }, { runners: [] }],
+		players: [
+			{
+				...player,
+				id: "player-0",
+				money: 15,
+				roundBetting: [],
+			},
+			{
+				...player,
+				id: "player-1",
+				money: 9,
+				roundBetting: [],
+			},
+		],
+	});
+});
+it.todo("resolveRoundBetReward set player money to zero if it goes below zero");
+
+it("betGame", () => {
+	expect(
+		betGame(
+			{
+				...gameState,
+				gameBetting: {
+					first: [],
+					last: [{ owner: "player-0", color: "blue" }],
+				},
+				players: [{ ...player, id: "player-0", gameBetting: ["red"] }],
+			},
+			"player-0",
+			"red",
+			"last",
+		),
+	).toEqual({
+		...gameState,
+		gameBetting: {
+			first: [],
+			last: [
+				{ owner: "player-0", color: "blue" },
+				{ owner: "player-0", color: "red" },
+			],
+		},
+		players: [{ ...player, id: "player-0", gameBetting: [] }],
+	});
+});
+it.todo(
+	"betGame throws an error if the player already made a bet for desired color",
+);
+
+it("resolveGameBetReward", () => {
+	expect(
+		resolveGameBetReward({
+			...gameState,
+			tracks: [{ runners: ["red"] }, { runners: ["blue"] }],
+			gameBetRewards: { first: [8], last: [5] },
+			gameBetPenalty: { first: -1, last: -1 },
+
+			gameBetting: {
+				first: [
+					{ owner: "player-0", color: "red" },
+					{ owner: "player-0", color: "blue" },
+					{ owner: "player-1", color: "blue" },
+				],
+				last: [{ owner: "player-1", color: "red" }],
+			},
+
+			players: [
+				{ ...player, id: "player-0", money: 10 },
+				{ ...player, id: "player-1", money: 10 },
+			],
+		}),
+	).toEqual({
+		...gameState,
+		tracks: [{ runners: ["red"] }, { runners: ["blue"] }],
+		gameBetRewards: { first: [8], last: [5] },
+		gameBetPenalty: { first: -1, last: -1 },
+
+		gameBetting: { first: [], last: [] },
+		players: [
+			{
+				...player,
+				id: "player-0",
+				money: 10 + 8 - 1,
+				gameBetting: expect.any(Array),
+			},
+			{
+				...player,
+				id: "player-1",
+				money: 10 + 0 + 5,
+				gameBetting: expect.any(Array),
+			},
+		],
+	});
+});
+
+it("setPartnership", () => {
+	expect(
+		setPartnership(
+			{
+				...gameState,
+				players: [
+					{ ...player, id: "player-0", partnership: undefined },
+					{ ...player, id: "player-1", partnership: undefined },
+				],
+			},
+			"player-0",
+			"player-1",
+		),
+	).toEqual({
+		...gameState,
+		players: [
+			{ ...player, id: "player-0", partnership: "player-1" },
+			{ ...player, id: "player-1", partnership: "player-0" },
+		],
+	});
+});
+it.todo("setPartnership throws an error if one of player already has partner");
+
+it("resolvePartnershipReward", () => {
+	expect(
+		resolvePartnershipReward({
+			...gameState,
+			tracks: [{ runners: ["blue", "red"] }],
+			players: [
+				{
+					...player,
+					id: "player-0",
+					partnership: "player-1",
+					money: 0,
+					rollTokens: [1],
+				},
+				{
+					...player,
+					id: "player-1",
+					partnership: "player-0",
+					money: 0,
+					roundBetting: [{ color: "red", value: [5, 1, -1] }],
+				},
+			],
+		}),
+	).toEqual({
+		...gameState,
+		tracks: [{ runners: ["blue", "red"] }],
+		players: [
+			{
+				...player,
+				id: "player-0",
+				rollTokens: [1],
+				partnership: undefined,
+				money: 5,
+			},
+			{
+				...player,
+				id: "player-1",
+				roundBetting: [{ color: "red", value: [5, 1, -1] }],
+				partnership: undefined,
+				money: 1,
+			},
+		],
+	});
+});
--- packages/core/src/gameState.ts
+++ packages/core/src/gameState.ts
@@ -20,6 +20,16 @@ export interface GameState<
 	RunnerColors extends string,
 	DiceColors extends string,
 > {
+	initialTrackLength: number;
+	gameBetRewards: {
+		first: number[];
+		last: number[];
+	};
+	gameBetPenalty: {
+		first: number;
+		last: number;
+	};
+
 	tracks: {
 		runners: RunnerColors[];
 		effectTile?: { type: 1 | -1; owner: string };
@@ -29,6 +39,8 @@ export interface GameState<
 		unrolled: UnrolledDice<DiceColors, RunnerColors>[];
 		rolled: RolledDice<DiceColors, RunnerColors>[];
 	};
+
+	rollTokens: number[];
 
 	gameBetting: {
 		first: { owner: string; color: RunnerColors }[];
@@ -42,15 +54,19 @@ export interface GameState<
 	players: {
 		id: string;
 		money: number;
-		hasEffectTile?: true;
+		hasEffectTile: boolean;
 		gameBetting: RunnerColors[];
 		roundBetting: RoundBetCard<RunnerColors>[];
 		partnership?: string;
 		isActive: boolean;
 		isRoundLead: boolean;
-		rollCount: number;
+		rollTokens: number[];
 	}[];
 }
+
+const getLeaderboard = <R extends string, D extends string>(
+	gameState: GameState<R, D>,
+): R[] => gameState.tracks.flatMap(({ runners }) => runners).toReversed();
 
 export const moveRunners = <R extends string, D extends string>(
 	gameState: GameState<R, D>,
@@ -75,16 +91,15 @@ export const moveRunners = <R extends string, D extends string>(
 };
 
 export const rollDice = <R extends string, D extends string>(
-	playerId: string,
 	gameState: GameState<R, D>,
-): {
-	gameState: GameState<R, D>;
-	diff: { dice: RolledDice<D, R>; player: { id: string; rollCount: number } };
-} => {
-	const newGameState = produce(gameState, (draft) => {
+	playerId: string,
+): GameState<R, D> => {
+	return produce(gameState, (draft) => {
 		const player = draft.players.find(({ id }) => playerId === id);
-
 		if (!player) throw new Error("player does not exist");
+
+		if (draft.dices.unrolled.length < 1)
+			throw new Error("no unrolled dice remains");
 
 		const diceIndex = Math.floor(Math.random() * draft.dices.unrolled.length);
 		const dice = draft.dices.unrolled[diceIndex];
@@ -98,92 +113,252 @@ export const rollDice = <R extends string, D extends string>(
 
 		draft.dices.rolled.push({ color: dice.color, prevResult: option });
 
-		player.rollCount++;
-	});
+		const rollToken = draft.rollTokens.pop();
+		if (!rollToken) throw new Error("no roll token remains");
 
-	return {
-		diff: {
-			// biome-ignore lint/style/noNonNullAssertion: an existence is guaranteed
-			dice: newGameState.dices.rolled.at(-1)!,
-			player: {
-				id: playerId,
-				// biome-ignore lint/style/noNonNullAssertion: an existence is guaranteed
-				rollCount: newGameState.players.find(({ id }) => playerId === id)!
-					.rollCount,
-			},
-		},
-		gameState: newGameState,
-	};
+		player.rollTokens.push(rollToken);
+	});
 };
 
 export const resolveDiceRollReward = <R extends string, D extends string>(
 	gameState: GameState<R, D>,
 ): GameState<R, D> => {
-	return gameState;
+	return produce(gameState, (draft) => {
+		draft.players.forEach((player) => {
+			player.money += player.rollTokens.reduce((sum, value) => sum + value, 0);
+			player.rollTokens = [];
+		});
+	});
 };
 
 export const setEffectTile = <R extends string, D extends string>(
 	gameState: GameState<R, D>,
+	playerId: string,
+	trackIndex: number,
+	type: 1 | -1,
 ): GameState<R, D> => {
-	return gameState;
+	return produce(gameState, (draft) => {
+		const player = draft.players.find(({ id }) => playerId === id);
+		if (!player) throw new Error("player does not exist");
+
+		player.hasEffectTile = false;
+
+		const prevSetTrack = draft.tracks.find(
+			({ effectTile }) => playerId === effectTile?.owner,
+		);
+		if (prevSetTrack) prevSetTrack.effectTile = undefined;
+
+		const targetTrack = draft.tracks.at(trackIndex);
+		if (!targetTrack) throw new Error("targetIndex is out of bound");
+
+		if (targetTrack.runners.length > 0)
+			throw new Error("targetTrack has runners on it");
+
+		if (
+			(draft.tracks[trackIndex + 1] &&
+				draft.tracks[trackIndex + 1].effectTile) ||
+			(draft.tracks[trackIndex - 1] && draft.tracks[trackIndex - 1].effectTile)
+		)
+			throw new Error("effectTile is already set in adjacent track");
+
+		targetTrack.effectTile = { owner: playerId, type };
+	});
+};
+
+export const isOnEffectTile = <R extends string, D extends string>(
+	gameState: GameState<R, D>,
+): { value: true; index: number } | { value: false; index: undefined } => {
+	const index = gameState.tracks.findIndex(
+		({ runners, effectTile }) => runners.length > 0 && !!effectTile,
+	);
+	return index === -1
+		? { value: false, index: undefined }
+		: { value: true, index };
 };
 
 export const resolveEffectTileReward = <R extends string, D extends string>(
 	gameState: GameState<R, D>,
 ): GameState<R, D> => {
-	return gameState;
-};
+	const { value, index } = isOnEffectTile(gameState);
+	if (!value) throw new Error("no track has runners on an effect tile");
 
-export const isOnEffectTile = <R extends string, D extends string>(
-	gameState: GameState<R, D>,
-): GameState<R, D> => {
-	return gameState;
+	return produce(gameState, (draft) => {
+		// biome-ignore lint/style/noNonNullAssertion: existence is guaranteed
+		const { owner, type } = draft.tracks[index].effectTile!;
+
+		const player = draft.players.find(({ id }) => owner === id);
+		if (!player) throw new Error("player does not exist");
+
+		player.money += type;
+	});
 };
 
 export const betRound = <R extends string, D extends string>(
 	gameState: GameState<R, D>,
+	playerId: string,
+	color: R,
 ): GameState<R, D> => {
-	return gameState;
+	return produce(gameState, (draft) => {
+		const player = draft.players.find(({ id }) => playerId === id);
+		if (!player) throw new Error("player does not exist");
+
+		const roundBetting = draft.roundBetting[color as string].pop();
+		if (!roundBetting)
+			throw new Error("no place for betting is left for desired runner color");
+
+		player.roundBetting.push(roundBetting);
+	});
 };
 
 export const resolveRoundBetReward = <R extends string, D extends string>(
 	gameState: GameState<R, D>,
+	sort: (a: RoundBetCard<R>, b: RoundBetCard<R>) => number = (a, b) =>
+		a.value[0] - b.value[0],
 ): GameState<R, D> => {
-	return gameState;
+	const ranks = Object.fromEntries(
+		getLeaderboard(gameState).map((color, index) => [color, index]),
+	);
+	return produce(gameState, (draft) => {
+		draft.players.forEach((player) => {
+			player.roundBetting.forEach((bet) => {
+				if (bet.value.length < 1)
+					throw new Error("roundBet has empty value cannot exist");
+
+				const rank = ranks[bet.color];
+				if (rank >= bet.value.length)
+					player.money += bet.value[bet.value.length - 1];
+				else player.money += bet.value[rank];
+
+				player.money = Math.max(0, player.money);
+
+				draft.roundBetting[bet.color as string].push(bet);
+			});
+			player.roundBetting = [];
+		});
+
+		Object.keys(draft.roundBetting).forEach((color) =>
+			draft.roundBetting[color].sort(sort),
+		);
+	});
 };
 
 export const betGame = <R extends string, D extends string>(
 	gameState: GameState<R, D>,
+	playerId: string,
+	color: R,
+	betType: "first" | "last",
 ): GameState<R, D> => {
-	return gameState;
+	return produce(gameState, (draft) => {
+		const player = draft.players.find(({ id }) => playerId === id);
+		if (!player) throw new Error("player does not exist");
+
+		if (!player.gameBetting.includes(color as Draft<R>))
+			throw new Error("player already made a bet for desired color");
+
+		player.gameBetting = player.gameBetting.filter(
+			(runnerColor) => color !== runnerColor,
+		);
+
+		draft.gameBetting[betType].push({
+			owner: playerId,
+			color: color as Draft<R>,
+		});
+	});
 };
 
 export const resolveGameBetReward = <R extends string, D extends string>(
 	gameState: GameState<R, D>,
 ): GameState<R, D> => {
-	return gameState;
+	const rewards = {
+		first: [...gameState.gameBetRewards.first],
+		last: [...gameState.gameBetRewards.last],
+	};
+	const [firstRunner] = getLeaderboard(gameState);
+	const [lastRunner] = getLeaderboard(gameState).toReversed();
+
+	return produce(gameState, (draft) => {
+		draft.gameBetting.first.forEach(({ owner, color }) => {
+			const player = draft.players.find(({ id }) => owner === id);
+			if (!player) throw new Error("player does not exist");
+
+			if (color === firstRunner) player.money += rewards.first.pop() ?? 0;
+			else player.money += gameState.gameBetPenalty.first;
+
+			player.gameBetting.push(color);
+		});
+		draft.gameBetting.first = [];
+
+		draft.gameBetting.last.forEach(({ owner, color }) => {
+			const player = draft.players.find(({ id }) => owner === id);
+			if (!player) throw new Error("player does not exist");
+
+			if (color === lastRunner) player.money += rewards.last.pop() ?? 0;
+			else player.money += gameState.gameBetPenalty.last;
+
+			player.gameBetting.push(color);
+		});
+		draft.gameBetting.last = [];
+	});
 };
 
 export const setPartnership = <R extends string, D extends string>(
 	gameState: GameState<R, D>,
+	playerId1: string,
+	playerId2: string,
 ): GameState<R, D> => {
-	return gameState;
+	return produce(gameState, (draft) => {
+		const player1 = draft.players.find(({ id }) => playerId1 === id);
+		if (!player1) throw new Error("player does not exist");
+
+		const player2 = draft.players.find(({ id }) => playerId2 === id);
+		if (!player2) throw new Error("player does not exist");
+
+		if (player1.partnership || player2.partnership)
+			throw new Error("one of player already has partner");
+
+		player1.partnership = playerId2;
+		player2.partnership = playerId1;
+	});
 };
 
 export const resolvePartnershipReward = <R extends string, D extends string>(
 	gameState: GameState<R, D>,
 ): GameState<R, D> => {
-	return gameState;
+	const ranks = Object.fromEntries(
+		getLeaderboard(gameState).map((color, index) => [color, index]),
+	);
+
+	return produce(gameState, (draft) => {
+		draft.players.forEach((player) => {
+			const partner = draft.players.find(({ id }) => player.partnership === id);
+			if (player.partnership && !partner)
+				throw new Error("player does not exist");
+			if (!partner) return;
+
+			const partnershipReward = Math.max(
+				...partner.rollTokens,
+				...partner.roundBetting.map(({ color, value }) => {
+					const rank = ranks[color];
+					if (rank >= value.length) return value[value.length - 1];
+					else return value[rank];
+				}),
+				0,
+			);
+
+			player.money += partnershipReward;
+			player.partnership = undefined;
+		});
+	});
 };
 
 export const isRoundEnded = <R extends string, D extends string>(
 	gameState: GameState<R, D>,
-): GameState<R, D> => {
-	return gameState;
+): boolean => {
+	return gameState.rollTokens.length === 0;
 };
 
 export const isGameEnded = <R extends string, D extends string>(
 	gameState: GameState<R, D>,
-): GameState<R, D> => {
-	return gameState;
+): boolean => {
+	return gameState.tracks.length > gameState.initialTrackLength;
 };
